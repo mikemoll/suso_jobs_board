e12435f03c116dca1c4196a8e8a076311b537bb5
Prelang Builder <builder@prelang.com>
2014-06-09 23:44:46 +0000

    Initial commit; clean Rails application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SusoJobsBoard::Application.config.secret_key_base = 'ae0d0cfee2e6e554ebd467d3d4068c7d88c6e629bceaa9db1f073577d80d99c14c95ec465cb31f5baa6d7d6073a5441b63170a5f5e95819ee90c057044748389'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SusoJobsBoard::Application.config.session_store :cookie_store, key: '_suso_jobs_board_session'
